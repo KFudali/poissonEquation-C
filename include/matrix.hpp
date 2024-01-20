16c8efa19b853c8ab063f5f1dc6f916342c415f9
Krystian Fudali <krystian.fudali42@gmail.com>
2024-01-20 11:32:19 +0100

    added assemblation of global matrix, added some methods to matrix class

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -8,14 +8,19 @@ template<typename T> class Matrix
         int row;
         int col;
         int vec_id;
-        std::vector<std::vector<T>> mat;
+
     public:
-        Matrix(T init_val, int r, int c) : row(r), col(c)
+
+        std::vector<std::vector<T>> mat;
+        Matrix(T init_val, int c, int r) :  col(c), row(r)
         {
             std::vector<T> row_vec(row, init_val);
             std::vector<std::vector<T>> matrix(col, row_vec);
             this->mat = matrix;
         };
+        Matrix(std::vector<std::vector<T>> init_matrix) : mat(init_matrix)
+        {};
+
         ~Matrix() {};
 
         void printMatrix()
@@ -23,7 +28,7 @@ template<typename T> class Matrix
             for_each(mat.begin(),
                      mat.end(),
                      [](const auto& column)
-                    {
+                    {                                
                         for_each(column.begin(),
                                  column.end(),
                                 [](const auto& elem)
@@ -33,9 +38,73 @@ template<typename T> class Matrix
                         std::cout<<std::endl;
                     });
         };
-
-        T operator()(int row_id, int col_id)
+        const T operator()(int row_id, int col_id)
         {
             return this->mat[row_id][col_id];
         };
+        Matrix operator+(const Matrix mat_2)
+        {
+            if(this->row != mat_2.col &&  this->col != mat_2.col)
+            {
+                std::cout<<"Matrices have different dimensions and cannot be added!" << std::endl;
+                return *this;
+            }
+            else
+            {
+                for(int i = 0; i < this->row; i++)
+                {
+                    for(int j = 0; j < this->col; j++)
+                    {
+                        this->mat[i][j] += mat_2.mat[i][j];
+                    }
+                }
+                return *this;
+            }
+        };
+        void set(int row_id, int col_id, T value)
+        {
+            this->mat[row_id][col_id] = value;
+        }
+        void setColumn(int col_num, const std::vector<T> column)
+        {
+            for(int i = 0; i < this->row; i++)
+            {
+                this->mat[i][col_num] = column[i];
+            }
+        };
+
+        void setRow(int row_num, const std::vector<T> row)
+        {
+           this->mat[row_num] = row;
+        };
+
+        std::vector<T> getColumn(int col_num)
+        {
+            std::vector<T> column(this->row);
+            for(int i = 0; i < this->row; i++)
+            {
+                column[i] = this->mat[i][col_num];
+            }
+            return column;
+        }
+        std::vector<T> getRow(int row_num)
+        {
+            return this->mat[row_num];
+        };
+        std::vector<T> operator*(const std::vector<T> vec)
+        {
+            std::vector<T> result(this->row);
+            for(int i = 0; i < this->row; i++)
+            {
+                for(int j = 0; j < this->col; j++)
+                {
+                  result[i] += this->mat[i][j]*vec[i];
+                }
+            }
+            return result;
+        }
+        Matrix operator*(const Matrix mat)
+        {
+            return *this;
+        }
 };
--- src/poissonEquation.cpp
+++ src/poissonEquation.cpp
@@ -1,11 +1,96 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <array>
 #include "../include/matrix.hpp"
 
 int main(){
 
-    Matrix mat(0., 3, 3);
-    // mat.printMatrix();
-    std::cout<< mat(1,1);
+    // FEM method (a bit sketchy since its taken from an old project done in MATLAB :| //
+
+    const double Ly = 1;
+    const double Lx = 1;
+    const int ny  = 4;
+    const int nx  = 4;
+    const int n_elements = (nx - 1) * (ny - 1);
+
+    Matrix K_e(2./3., 4, 4);
+
+    std::vector<double> element_stiffnes =  {2./3., -1./6., -1./3., -1./6.,
+                                            -1./6., 2./3., -1./6., -1./3.,
+                                            -1./3., -1./6., 2./3., -1./6.,
+                                            -1./6., -1./3., -1./6., 2./3.};
+
+    // for_each(element_stiffnes.begin(), element_stiffnes.end(), [](const auto& elem){std::cout<<elem;});
+
+    for(int i = 0; i < 4; i++)
+    {
+         for(int j = 0; j < 4; j++)
+        {
+            K_e.set(i,j,element_stiffnes[(i*4)+j]);
+        }
+    };
+
+    // K_e.printMatrix();
+
+    // for_each(row.begin(), row.end(), [](const auto& elem){std::cout<<elem;});
+
+    Matrix element_nodes_ids(0, n_elements, 4);
+    int first_node;
+    std::vector<int> ids_vector;
+
+    for(int i = 0; i < n_elements; i++)
+    {
+        first_node = i + (int)floor((double)i/((double)(nx-1)));
+
+        ids_vector = {first_node, first_node + 1,  first_node + nx + 1, first_node + nx};
+        element_nodes_ids.setRow(i, ids_vector);
+    }
+
+    Matrix K_g(0., nx*nx, ny*ny);
+    // assemble global matrix
+    double val;
+    K_e.printMatrix();
+    std::cout<<std::endl;
+    for(int i = 0; i < n_elements; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                val = K_g(element_nodes_ids(i,k),element_nodes_ids(i,j));
+                K_g.set(element_nodes_ids(i,k),element_nodes_ids(i,j), val + K_e(j,k));
+            }
+        }
+    }
+    K_g.printMatrix();
+    std::cout<<std::endl;
+
+    std::vector<int> bottom_ids(nx, 0);
+    std::vector<int> top_ids(nx, 0);
+    std::vector<int> left_ids(ny, 0);
+    std::vector<int> right_ids(ny, 0);
+
+    for(int i = 0; i < nx; i++)
+    {
+        bottom_ids[i] = i;
+        top_ids[i] = (nx * (ny - 1)) + i;
+        left_ids[i] = i*nx;
+        right_ids[i] = (i+1)*nx;
+    };
+
+    std::vector<double> u(nx*ny, 1.);
+    std::vector<double> rhs(nx*ny, 1.);
+
+    for(int i = 0; i < nx; i++)
+    {
+        u[bottom_ids[i]] = 1;
+    }
+    rhs = K_g*u;
+
+    // deleting elements from global stifness matrix - needed for dirichlet BC
+
+
+
 
 } 
